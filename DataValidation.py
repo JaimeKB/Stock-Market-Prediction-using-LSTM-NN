9d252e7da450bd41b7820c479aab7fef0125a54d
unknown <jaime.kershawbrown@students.plymouth.a.cuk>
2021-04-24 18:02:12 +0100

    Testing setup for distribution to fix file pathing and module not found error

---
+++ DataValidation.py
@@ -0,0 +1,208 @@
+import math
+import tensorflow.keras
+import pandas as pd
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dropout
+#from tensorflow.keras.layers import *
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.callbacks import EarlyStopping
+import datetime
+import os
+
+
+def validateDate(date_text):
+    result = ""
+    try:
+        datetime.datetime.strptime(date_text, "%d/%m/%Y") # "%Y-%m-%d" for all stocks folder
+    except:
+        result = "Fail"
+        print("Incorrect date format, should be DD-MM-YYYY") 
+
+def validateNumbers(data):
+    result = ""
+    
+    try:
+        if(isinstance(float(data[0]), float) and isinstance(float(data[1]), float) and isinstance(float(data[2]), float) and isinstance(float(data[3]), float) and isinstance(int(data[4]), int)):
+            pass
+        else:
+            print("Number data does not match required format.")
+            result = "Fail"
+    except:
+        print("Number data does not match required format.")
+        result = "Fail"
+
+    return result
+
+def ValidateYahooCSVData(filePath, filename):
+    """
+    Check that data in file follows required format and won't cause any crashes.
+    Required format: Date,Open,High,Low,Close,Volume,OpenInt
+    Must not have any missing fields
+    """
+
+    # Check if file is empty, if is return file fail
+    if(os.stat(filePath+"/"+filename).st_size == 0):
+        print("File is empty!")
+        return("Fail", 0)
+    else:
+        df=pd.read_csv(filePath+"/"+filename, sep=",")
+        dataSet = df.iloc[:]
+        if(len(dataSet.index) < 132):
+            print("Not enough data in file!")
+            return("Fail", 0)
+        else:
+            for index, row in dataSet.iterrows():
+                validateDate(row['Date'])             
+                if(pd.isna(row['Open']) or pd.isna(row['High']) or pd.isna(row['Low']) or pd.isna(row['Close']) or pd.isna(row['Volume'])):
+                    print("Null data in file")
+                    return("Fail", 0)
+                else:
+                    data = [row['Open'], row['High'], row['Low'], row['Close'], row['Volume']]
+                    result2 = validateNumbers(data)
+                    if(result2 == "Fail"):
+                        print("Numbers were not right type")
+                        return("Fail", 0) 
+                    
+    return("Pass", len(dataSet.index))
+
+def ValidateNasdaqData(filename):
+    """
+    Check Nasdaq csv data file follows required format and won't cause any crashes.
+    Required format: Date,Open,High,Low,Close,Volume,OpenInt
+    Must not have any missing fields
+    """
+    df=pd.read_csv("C:/Users/Jaime Kershaw Brown/Documents/Final year project/stock_comparison/nasdaq/"+filename)
+    dataSet = df.iloc[:]
+    fileResult = ""
+    print(dataSet)
+    for index, row in dataSet.iterrows():
+        # result = validateDate(row['Date']) 
+        if(row[' Close/Last'] == " N/A" or row[' Volume'] == " N/A" or row[' Open'] == " N/A" or row[' High'] == " N/A" or row[' Low'] == " N/A"):
+            pass
+        else:
+            data = [float(row[' Close/Last'][2:]), int(row[' Volume']), float(row[' Open'][2:]), float(row[' High'][2:]), float(row[' Low'][2:])]
+
+    # print(data)
+    # print(type(data[3]))
+    
+    #     result2 = validateNumbers(data)
+    #     if(result == "Fail" or result2 == "Fail"):
+    #         fileResult = "Fail"
+    # if(fileResult == "Fail"):
+    #     print("This file fails")
+    # else:
+    #     print("This file passes")    
+
+def ValidateTXTData(filename):
+    """
+    Check that data in txt file follows required format and won't cause any crashes.
+    Required format: Date,Open,High,Low,Close,Volume,OpenInt
+    Must not have any missing fields
+    """
+    fileResult = ""
+    if(os.stat('C:/Users/Jaime Kershaw Brown/Documents/Final year project/huge_stock_market_dataset/Stocks/'+filename).st_size == 0):
+        print("File is empty!")
+        fileResult = "Fail"
+    else:
+        dataSet = pd.read_csv('C:/Users/Jaime Kershaw Brown/Documents/Final year project/huge_stock_market_dataset/Stocks/'+filename, sep=",")
+        dataSet.columns = ["Date", "Open", "High", "Low", "Close", "Volume", "OpenInt"]
+        
+        if(len(dataSet.index) < 132):
+            print("Not enough data in file!")
+            fileResult = "Fail"
+        else:
+            for index, row in dataSet.iterrows():
+                validateDate(row['Date']) 
+                data = [row['Open'], row['High'], row['Low'], row['Close'], row['Volume']]        
+                result2 = validateNumbers(data)
+                if(result2 == "Fail"):
+                    fileResult = "Fail"
+
+    if(fileResult == "Fail"):
+        print("Fail")
+        return "Fail"
+    else:
+        return "Pass"
+
+
+def LoopThroughFiles():
+    """
+    Function to loop through files for validation
+    """
+    filePath = "C:/Users/Jaime Kershaw Brown/Documents/Final year project/stocks"
+    failedFiles = 0
+    passedFiles = 0
+    fileLineNumbers = 0
+    totalLineNumbers = 0
+    now = datetime.datetime.now()
+
+    StartTime = now.strftime("%H:%M:%S")
+
+    directory = os.fsencode(filePath)
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".csv"): 
+            print(filename)
+            result, fileLineNumbers = ValidateYahooCSVData(filePath, filename)
+            if(result == "Fail"):
+                print("This file fails")
+                failedFiles +=1
+                if os.path.exists(filePath+"/"+filename):
+                    os.remove(filePath+"/"+filename)
+                else:
+                    print("The file does not exist")
+            else:
+                print("this file passes")
+                passedFiles +=1
+                totalLineNumbers += fileLineNumbers
+                fileLineNumbers = 0
+        else:
+            pass
+
+    now = datetime.datetime.now()
+    endTime = now.strftime("%H:%M:%S")
+
+    print("Start Time =", StartTime)
+    print("End Time =", endTime)
+    print("Passed files:", passedFiles)
+    print("Failed files:", failedFiles)
+    print("Total line numbers:", totalLineNumbers)
+
+def OrganiseTestingData(df): 
+
+    storedTrainingSet = pd.read_csv('C:/Users/Jaime Kershaw Brown/Documents/Final year project/Stock-Market-Prediction-using-LSTM-NN/StockModel2/trainingData.txt', header = None)
+    trainingDataShape = storedTrainingSet.shape
+
+    sc = MinMaxScaler(feature_range = (0, 1))
+    trainingData = np.loadtxt("C:/Users/Jaime Kershaw Brown/Documents/Final year project/Stock-Market-Prediction-using-LSTM-NN/StockModel2/trainingData.txt").reshape(trainingDataShape[0], trainingDataShape[1])
+    sc.fit_transform(trainingData)
+    testingDataLength = math.floor(len(df.iloc[:, 1:2]))
+
+    tempVal1 = df.iloc[:10, 1:2]
+    tempVal2 = df.iloc[10:, 1:2]
+
+    dataSet = df.iloc[:, 1:2]
+
+    dataset_total = pd.concat((tempVal1, tempVal2), axis = 0)
+    inputs = dataset_total.values
+
+    inputs = inputs.reshape(-1, 1)
+    inputs = sc.transform(inputs)
+    x_test = []
+    for i in range(60, testingDataLength):
+        x_test.append(inputs[i-60:i, 0])
+    x_test = np.array(x_test)
+
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+    return x_test
+
+
+if __name__ == "__main__":
+    LoopThroughFiles()
---
+++ server.py
@@ -0,0 +1,109 @@
+from flask import Flask
+from flask import render_template
+from flask import jsonify
+from flask import request
+from pandas import DataFrame
+from DataValidation import OrganiseTestingData
+from DataValidation import ValidateYahooCSVData
+from TestModel import PredictData
+from TestModel import TestUserModel
+from TestModel import RunOwn
+import numpy as np
+import pandas as pd
+import os
+
+Myapp = Flask(__name__)
+
+def to_matrix(l):
+    return [l[i:i+7] for i in range(0, len(l), 7)]
+
+@Myapp.route("/Index")
+def hello():
+    return render_template('Index.html')
+
+@Myapp.route("/TermsOfService")
+def Redirect():
+    return render_template('TermsOfService.html')
+
+@Myapp.route('/uploadajax', methods = ['POST'])
+def upldfile():
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            uploaded_file.save(os.path.join('C:/Users/Jaime Kershaw Brown/Documents/Final year project/Stock-Market-Prediction-using-LSTM-NN/StockModel2/userModels', "userModel.h5"))
+            predictedData, dateRange = TestUserModel()
+
+            myModelPredictedData = RunOwn()
+
+            testDict = {
+                "predictedData": predictedData.tolist(),
+                "myModelPredictedData": myModelPredictedData.tolist(),
+                "dateRange": dateRange.tolist()
+            }
+
+            return jsonify(testDict)
+
+    return("Success")
+
+
+@Myapp.route('/uploadCSV', methods = ['POST'])
+def uploadCSVFile():
+    if request.method == 'POST':
+        uploaded_file = request.files['CSVfile']
+        if uploaded_file.filename != '':
+            uploaded_file.save(os.path.join('C:/Users/Jaime Kershaw Brown/Documents/Final year project/Stock-Market-Prediction-using-LSTM-NN/StockModel2/userCSVFiles', "userCSV.csv"))
+
+            result, length = ValidateYahooCSVData("C:/Users/Jaime Kershaw Brown/Documents/Final year project/Stock-Market-Prediction-using-LSTM-NN/StockModel2/userCSVFiles", "userCSV.csv")
+
+            if(result == "Pass"):
+
+                df=pd.read_csv("C:/Users/Jaime Kershaw Brown/Documents/Final year project/Stock-Market-Prediction-using-LSTM-NN/StockModel2/userCSVFiles/userCSV.csv")
+                testData = OrganiseTestingData(df)
+                predictedData = PredictData(testData)
+
+                dateRange = df.loc[60:, 'Date']
+
+                testDict = {
+                    "message": "Data was received!",
+                    "predictedData": predictedData.tolist(),
+                    "testData": testData.tolist(),
+                    "dateRange": dateRange.tolist(),
+                    "dataLength": len(testData)
+                }
+
+                return jsonify(testDict)
+
+            else:
+                return("Uploaded file was unusable!")
+
+    return("Success")
+
+
+
+
+# @Myapp.route('/stockDataFile/<stockData>',methods=['GET'])
+# def ProcessStockData(stockData):
+#     print("Stock data recieved:")
+#     stockData = stockData.split(",")
+#     twoDStockData = to_matrix(stockData)
+#     twoDStockData.pop(0)
+#     df = DataFrame (twoDStockData,columns=['Date','Open','High','Low','Close','Adj Close','Volume'])
+
+#     testData = OrganiseTestingData(df)
+#     predictedData = PredictData(testData)
+
+#     dateRange = df.loc[60:, 'Date']
+
+#     testDict = {
+#         "message": "Data was received!",
+#         "predictedData": predictedData.tolist(),
+#         "testData": testData.tolist(),
+#         "dateRange": dateRange.tolist(),
+#         "dataLength": len(testData)
+#     }
+
+#     return jsonify(testDict)
+
+
+if __name__ == "__main__":
+    Myapp.run()
